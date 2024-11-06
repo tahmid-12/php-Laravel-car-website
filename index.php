45e4966cc074270d263145a10933ab7b41f057e5
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-11-06 17:00:29 +0600

    First push

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Education Site</title>
+    <?php include('./client/commonFiles.php'); ?>
+</head>
+<body>
+
+    <?php include('./client/header.php') ?>
+    <!-- 28:20 Seconds -->
+    
+    <?php include('./client/footer.php'); ?>
+</body>
+</html>
