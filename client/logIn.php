d339d1d24ec3d17584cd67905ae87c336e954b80
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-11-06 17:50:45 +0600

    SIgn Up and Log In Pages Created

---
+++ client/logIn.php
@@ -0,0 +1,28 @@
+<div class="container">
+    <h1 class="heading">Log In</h1>
+</div>
+
+<form>
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email" placeholder="Enter Email">
+    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+  </div>
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" placeholder="Enter Password">
+    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+  </div>
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Always Remember</label>
+  </div>
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+        <button type="submit" class="btn btn-primary">Log In</button>
+  </div>
+
+</form>
