9533be893238cc93f5796f6d273ea8bbd577e00b
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-11-13 00:29:11 +0600

    Sign Up Form Created

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,4 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class,'index']);
+Route::get('sign-up',[SignupController::class, 'create']);
