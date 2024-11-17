aad4d3e70242957161626b639e6332fa1d2cbfd1
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-11-17 23:59:37 +0600

    Table in progress

---
+++ database/migrations/2024_11_17_173149_create_models_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('models', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('maker_id')->constrained('makers');
+            $table->string('name',45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('models');
+    }
+};
