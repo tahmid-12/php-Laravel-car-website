d51f456601c406dfa3bc48ff97e5bf0ef3c7b26a
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-11-20 09:52:57 +0600

    CArs Object added in Car Controller

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cars;
 use Illuminate\Http\Request;
 
 class CarController extends Controller
@@ -43,7 +44,7 @@ class CarController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Cars $car)
     {
         return view('car.show');
     }
@@ -54,7 +55,7 @@ class CarController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Cars $car)
     {
         return view('car.edit');
     }
@@ -66,7 +67,7 @@ class CarController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Cars $car)
     {
         //
     }
@@ -77,7 +78,7 @@ class CarController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Cars $car)
     {
         //
     }
