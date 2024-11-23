5f5aacf64af8bfeac6538fc53dfbb74f2d55b608
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-11-23 23:56:13 +0600

    Cars table Relation

--- app/Models/Cars.php
+++ app/Models/Cars.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cars extends Model
@@ -19,11 +22,31 @@ class Cars extends Model
         'mileage',
         'car_type_id',
         'fuel_type_id',
-        // 'user_id',
+        'user_id',
         'city_id',
         'address',
         'phone',
         'description',
         'published_at'
     ];
+
+    public function features(): HasOne
+    {
+        return $this->hasOne(CarFeatures::class);
+    }
+
+    public function primaryImage(): HasOne
+    {
+        return $this->hasOne(CarImages::class)->oldestOfMany('position');
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(CarImages::class);
+    }
+
+    public function carType(): BelongsTo
+    {
+        return $this->belongsTo(CarTypes::class);
+    }
 }
